ddc1de1421aa87cbfe330e627ea7fe6120d9c989
Ronan Drost <ronandrost20@gmail.com>
2024-04-09 21:45:38 +0200

    Update page layout to include header, playlists, main board, music info, and currently playing footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import Link from 'next/link'
 
 export default function Page() {
-  return <Link href="/playlists">Playlists</Link>
+  return <div>
+    <div className="grid grid-cols-5 grid-rows-5 gap-4">
+      <div className="col-span-5">Header</div>
+      <div className="row-span-3 row-start-2">Playlists</div>
+      <div className="col-span-3 row-span-3 row-start-2">Main Board</div>
+      <div className="row-span-3 col-start-5 row-start-2">Music Info</div>
+      <div className="col-span-5 row-start-5">Currently Playing footer</div>
+    </div>
+  </div>
 }
