027c13f1876ec3903e8e5f30dc0baa6c2cf1e665
Ronan Drost <ronandrost20@gmail.com>
2024-04-11 00:16:33 +0200

    Refactor file paths and import statements to match new directory structure

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,8 @@ import { BiSearch } from 'react-icons/bi';
 import { HiHome } from 'react-icons/hi';
 
 import Box from './Box';
+import SidebarItem from './SidebarItem';
+import Libary from './Library';
 
 interface SidebarProps {
     children: React.ReactNode;
@@ -33,11 +35,35 @@ const Sidebar: React.FC<SidebarProps> = ({
 
     return (
         <div className="flex h-full">
-            <div className="hidden md:flex flex-col gap-y-2 bg-black w-[300px] p-2">
+            <div 
+                className="
+                    hidden 
+                    md:flex 
+                    flex-col 
+                    gap-y-2 
+                    bg-black 
+                    h-full 
+                    w-[300px] 
+                    p-2
+                "
+            >
                 <Box>
-                    Sidebar Navigation
+                    <div className="flex flex-col gap-y-4 px-5 py-4">
+                        {routes.map((item) => (
+                            <SidebarItem 
+                                key={item.label}
+                                {...item}
+                            />    
+                        ))}
+                    </div>
+                </Box>
+                <Box className='overflow-y-auto h-full'>
+                    <Libary />
                 </Box>
             </div>
+            <main className='h-full flex-1 overflow-y-auto py-2'>
+                {children}
+            </main>
         </div>
     );
 }
