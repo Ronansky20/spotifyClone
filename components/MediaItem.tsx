89a472081a07ca0a647440ae05975909b46e7a28
Ronan Drost <ronandrost20@gmail.com>
2024-04-14 08:40:15 +0200

    Refactor MediaItem component to remove unused code and improve readability

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -33,6 +33,14 @@ const MediaItem: React.FC<MediaItemProps> = ({
                     className="object-cover"
                 />
             </div>
+            <div className="flex flex-col gap-y-1 overflow-hidden"> 
+                <p className="text-white truncate">
+                    {data.title}
+                </p>
+                <p className="text-neutral-400 text-sm truncate">
+                    By {data.author}
+                </p>
+            </div>
         </div>
      );
 }
