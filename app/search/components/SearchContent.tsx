af2d77e47b1a88a46340dd849cf5c36e5a2d12f1
Ronan Drost <ronandrost20@gmail.com>
2024-04-15 21:15:23 +0200

    Refactor SearchContent component to display songs using MediaItem component

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import MediaItem from "@/components/MediaItem";
 import { Song } from "@/types";
 
 interface SearchContentProps {
@@ -18,8 +19,21 @@ const SearchContent: React.FC<SearchContentProps> = (
     }
 
     return ( 
-        <div>
-            Search Content
+        <div className="flex flex-col gap-y-2 w-full px-6">
+            {songs.map((song) =>(
+                <div
+                    key={song.id}
+                    className="flex items-center gap-x-4 w-full"
+                >
+                    <div className="flex-1">
+                        <MediaItem 
+                            onClick={() => {}}
+                            data={song}
+                        />
+                    </div>
+                    {/* TODO:Add Like Button Here */}
+                </div>
+            ))}
         </div>
      );
 }
